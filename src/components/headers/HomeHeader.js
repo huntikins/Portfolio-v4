ff69b53b5f3642f178b6e4c8174f07afa8c147d1
htrammell <hunterttrammell@gmail.com>
2019-09-06 15:42:59 -0500

    completed home page

--- src/components/headers/HomeHeader.js
+++ src/components/headers/HomeHeader.js
@@ -5,34 +5,21 @@ class HomeHeader extends React.Component {
     return (
       <div className="home">
         <div className="row">
-          <div className=" col-4">
+          <div className="col-md-2 col-sm-12 text-left">
             <img
-              className="img-responsive w-100"
-              src="https://via.placeholder.com/300x300"
+              className="img-responsive home__image"
+              src="logo.png"
               alt="..."
             />
           </div>
-          <div className="col-md-4 col-sm-8">
-            <h1>Hunter Trammell</h1>
-          </div>
         </div>
-        <div className="row">
-          <div className="col">
-            <p>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab illo inventore veritatis et quasi architecto beatae vitae
-              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-              est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
-              velit, sed quia non numquam eius modi tempora incidunt ut labore
-              et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
-              veniam, quis nostrum exercitationem ullam corporis suscipit
-              laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
-              vel eum iure reprehenderit qui in ea voluptate velit esse quam
-              nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-              voluptas nulla pariatur?
+        <div className="row home__wrapper">
+          <div className="offset-md-2 col-md-8 col-sm-12">
+            <h1 className="home__header home__header-main">HUNTER TRAMMELL</h1>
+          </div>
+          <div className="offset-md-2 col-md-8 col-sm-12">
+            <p className="home__header home__header-sub">
+              FULL-STACK WEB DEVELOPER
             </p>
           </div>
         </div>
