f15b7a1c9a16b41c0cb6b9c827d88a79cb5e79d6
htrammell <hunterttrammell@gmail.com>
2019-09-08 21:17:32 -0500

    completed skill area other than mobile styling. added state switch and used onblur & timeouts to manage resetting skill wheel

--- src/components/IconWheel.js
+++ src/components/IconWheel.js
@@ -1,20 +1,210 @@
 import React from "react";
 
 class IconWheel extends React.Component {
-  state = {
-    skill: {
-      name: "Click an icon to view proficiency",
-      level: 0,
-      certification: {
-        item1: "KU Coding Bootcamp",
-        item2: "Free Code Camp"
-      }
-    }
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      skill: {
+        name: "Hunter Trammell",
+        level: 100,
+        certification: {
+          item1: "Click an icon to see skill level"
+        }
+      },
+      showImage: 1
+    };
+  }
   handleClick = tech => {
-    console.log(tech + " has been clicked");
     //match value passed into click event with index from state to display information
     //value will be presented in last li in ul
+    switch (tech) {
+      case "react":
+        this.setState({
+          skill: {
+            name: "React",
+            level: 60,
+            certification: {
+              item1: "Advanced"
+            }
+          },
+          showImage: 0
+        });
+        break;
+      case "html5":
+        this.setState({
+          skill: {
+            name: "HTML",
+            level: 95,
+            certification: {
+              item1: "Expert"
+            }
+          },
+          showImage: 0
+        });
+        break;
+      case "git":
+        this.setState({
+          skill: {
+            name: "GIT",
+            level: 70,
+            certification: {
+              item1: "Advanced"
+            }
+          },
+          showImage: 0
+        });
+        break;
+      case "aws":
+        this.setState({
+          skill: {
+            name: "AWS",
+            level: 30,
+            certification: {
+              item1: "Beginner"
+            }
+          },
+          showImage: 0
+        });
+        break;
+      case "node":
+        this.setState({
+          skill: {
+            name: "Node",
+            level: 70,
+            certification: {
+              item1: "Advanced"
+            }
+          },
+          showImage: 0
+        });
+        break;
+      case "css3":
+        this.setState({
+          skill: {
+            name: "CSS",
+            level: 90,
+            certification: {
+              item1: "Expert"
+            }
+          },
+          showImage: 0
+        });
+        break;
+      case "mongodb":
+        this.setState({
+          skill: {
+            name: "MongoDB",
+            level: 50,
+            certification: {
+              item1: "Intermediate"
+            }
+          },
+          showImage: 0
+        });
+        break;
+      case "sql":
+        this.setState({
+          skill: {
+            name: "SQL",
+            level: 65,
+            certification: {
+              item1: "Advanced"
+            }
+          },
+          showImage: 0
+        });
+        break;
+      case "js":
+        this.setState({
+          skill: {
+            name: "Javascript",
+            level: 75,
+            certification: {
+              item1: "Advanced"
+            }
+          },
+          showImage: 0
+        });
+        break;
+      case "wpress":
+        this.setState({
+          skill: {
+            name: "Wordpress",
+            level: 55,
+            certification: {
+              item1: "Intermediate"
+            }
+          },
+          showImage: 0
+        });
+        break;
+      case "php":
+        this.setState({
+          skill: {
+            name: "PHP",
+            level: 40,
+            certification: {
+              item1: "intermediate"
+            }
+          },
+          showImage: 0
+        });
+        break;
+      case "vuejs":
+        this.setState({
+          skill: {
+            name: "Vue",
+            level: 75,
+            certification: {
+              item1: "Intermediate"
+            }
+          },
+          showImage: 0
+        });
+        break;
+      case "sass":
+        this.setState({
+          skill: {
+            name: "Sass",
+            level: 80,
+            certification: {
+              item1: "Advanced"
+            }
+          },
+          showImage: 0
+        });
+        break;
+      case "adobe":
+        this.setState({
+          skill: {
+            name: "Adobe CC",
+            level: 40,
+            certification: {
+              item1: "Photoshop/Illustrator"
+            }
+          },
+          showImage: 0
+        });
+        break;
+      default:
+    }
+  };
+  componentDidUpdate() {
+    clearTimeout(this.reset);
+  }
+  renderDefault = () => {
+    this.reset = setTimeout(() => {
+      this.setState({
+        skill: {
+          name: "Hunter Trammell",
+          level: 100,
+          certification: {
+            item1: "Click an icon to see skill level"
+          }
+        },
+        showImage: 1
+      });
+    }, 1000);
   };
   render() {
     return (
@@ -23,61 +213,139 @@ class IconWheel extends React.Component {
           className="skill__list-item skill__list-item--react"
           onClick={() => this.handleClick("react")}
           tabIndex="0"
+          onBlur={() => this.renderDefault()}
         >
           <i className="skill__list-item--icon fab fa-react"></i>
         </li>
-        <li className="skill__list-item skill__list-item--html5" tabIndex="0">
+        <li
+          className="skill__list-item skill__list-item--html5"
+          onClick={() => this.handleClick("html5")}
+          tabIndex="0"
+          onBlur={() => this.renderDefault()}
+        >
           <i className="skill__list-item--icon fab fa-html5"></i>
         </li>
-        <li className="skill__list-item skill__list-item--git" tabIndex="0">
+        <li
+          className="skill__list-item skill__list-item--git"
+          onClick={() => this.handleClick("git")}
+          tabIndex="0"
+          onBlur={() => this.renderDefault()}
+        >
           <i className="skill__list-item--icon fab fa-git"></i>
         </li>
-        <li className="skill__list-item skill__list-item--aws" tabIndex="0">
+        <li
+          className="skill__list-item skill__list-item--aws"
+          onClick={() => this.handleClick("aws")}
+          tabIndex="0"
+          onBlur={() => this.renderDefault()}
+        >
           <i className="skill__list-item--icon fab fa-aws"></i>
         </li>
-        <li className="skill__list-item skill__list-item--node" tabIndex="0">
+        <li
+          className="skill__list-item skill__list-item--node"
+          onClick={() => this.handleClick("node")}
+          tabIndex="0"
+          onBlur={() => this.renderDefault()}
+        >
           <i className="skill__list-item--icon fab fa-node"></i>
         </li>
-        <li className="skill__list-item skill__list-item--css3" tabIndex="0">
+        <li
+          className="skill__list-item skill__list-item--css3"
+          onClick={() => this.handleClick("css3")}
+          tabIndex="0"
+          onBlur={() => this.renderDefault()}
+        >
           <i className="skill__list-item--icon fab fa-css3-alt"></i>
         </li>
-        <li className="skill__list-item skill__list-item--mongodb" tabIndex="0">
+        <li
+          className="skill__list-item skill__list-item--mongodb"
+          onClick={() => this.handleClick("mongodb")}
+          tabIndex="0"
+          onBlur={() => this.renderDefault()}
+        >
           <i className="skill__list-item--icon fas fa-server"></i>
         </li>
-        <li className="skill__list-item skill__list-item--sql" tabIndex="0">
+        <li
+          className="skill__list-item skill__list-item--sql"
+          onClick={() => this.handleClick("sql")}
+          tabIndex="0"
+          onBlur={() => this.renderDefault()}
+        >
           <i className="skill__list-item--icon fas fa-database"></i>
         </li>
-        <li className="skill__list-item skill__list-item--js" tabIndex="0">
+        <li
+          className="skill__list-item skill__list-item--js"
+          onClick={() => this.handleClick("js")}
+          tabIndex="0"
+          onBlur={() => this.renderDefault()}
+        >
           <i className="skill__list-item--icon fab fa-js"></i>
         </li>
-        <li className="skill__list-item skill__list-item--wpress" tabIndex="0">
+        <li
+          className="skill__list-item skill__list-item--wpress"
+          onClick={() => this.handleClick("wpress")}
+          tabIndex="0"
+          onBlur={() => this.renderDefault()}
+        >
           <i className="skill__list-item--icon fab fa-wordpress-simple"></i>
         </li>
-        <li className="skill__list-item skill__list-item--php" tabIndex="0">
+        <li
+          className="skill__list-item skill__list-item--php"
+          onClick={() => this.handleClick("php")}
+          tabIndex="0"
+          onBlur={() => this.renderDefault()}
+        >
           <i className="skill__list-item--icon fab fa-php"></i>
         </li>
-        <li className="skill__list-item skill__list-item--vuejs" tabIndex="0">
+        <li
+          className="skill__list-item skill__list-item--vuejs"
+          onClick={() => this.handleClick("vuejs")}
+          tabIndex="0"
+          onBlur={() => this.renderDefault()}
+        >
           <i className="skill__list-item--icon fab fa-vuejs"></i>
         </li>
-        <li className="skill__list-item skill__list-item--sass" tabIndex="0">
+        <li
+          className="skill__list-item skill__list-item--sass"
+          onClick={() => this.handleClick("sass")}
+          tabIndex="0"
+          onBlur={() => this.renderDefault()}
+        >
           <i className="skill__list-item--icon fab fa-sass"></i>
         </li>
-        <li className="skill__list-item skill__list-item--adobe" tabIndex="0">
+        <li
+          className="skill__list-item skill__list-item--adobe"
+          onClick={() => this.handleClick("adobe")}
+          tabIndex="0"
+          onBlur={() => this.renderDefault()}
+        >
           <i className="skill__list-item--icon fab fa-adobe"></i>
+        </li>
+        <li
+          className="skill__list-item--image"
+          tabIndex="0"
+          style={{
+            opacity: this.state.showImage,
+            transition: "opacity .2s ease-out"
+          }}
+        >
+          <img
+            className="img-responsive skill__list-image"
+            src="./hunterPic.png"
+            alt="Hunter Trammell"
+          />
         </li>
         <li className="skill__list-info">
           <h2 className="skill__list-header">{this.state.skill.name}</h2>
           <div className="skill__list-proficiency">
-            <span className="skill__list-proficiency--level"></span>
+            <span
+              className="skill__list-proficiency--level"
+              style={{ width: this.state.skill.level + "%" }}
+            ></span>
           </div>
           <ul className="skill__list-certification">
             <li className="skill__list-certification--item">
-              <i class="fas fa-graduation-cap"></i>{" "}
               {this.state.skill.certification.item1}
-            </li>
-            <li className="skill__list-certification--item">
-              <i class="fab fa-free-code-camp"></i>{" "}
-              {this.state.skill.certification.item2}
             </li>
           </ul>
         </li>
