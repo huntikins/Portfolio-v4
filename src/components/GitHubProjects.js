5c9dcc4c6a28a70c8a901734d969c319bfcd4bc2
htrammell <hunterttrammell@gmail.com>
2019-09-09 17:37:57 -0500

    completed portfolio, made coming soon for blog, deployed to netlify:

--- src/components/GitHubProjects.js
+++ src/components/GitHubProjects.js
@@ -8,13 +8,14 @@ class GitHub extends React.Component {
   componentDidMount() {
     axios({
       method: "get",
-      url: "https://api.github.com/user/repos?sort=created&visibility=public",
+      url: "https://api.github.com/user/repos?sort=updated&visibility=public",
       auth: {
         username: process.env.REACT_APP_GITHUB_USER,
         password: process.env.REACT_APP_GITHUB_PASS
       }
     }).then(res => {
       console.log(res.data);
+      this.setState({ repo: res.data });
       //push all repo's with data needed to state, show in a tile list with info from Github
       /*
       //name: "",
@@ -28,7 +29,42 @@ class GitHub extends React.Component {
     });
   }
   render() {
-    return <h1>Hello World!</h1>;
+    return (
+      <div className="github__wrapper">
+        <table className="github__table">
+          <tbody className="github__table-body">
+            {this.state.repo.map((item, index) => {
+              return (
+                <tr className="github__table-body--row" key={index}>
+                  <td className="github__table-body--content github__table-body--name">
+                    {item.name}
+                  </td>
+                  <td className="github__table-body--content github__table-body--description">
+                    {item.description}
+                  </td>
+                  <td className="github__table-body--content">
+                    <a
+                      href={item.html_url}
+                      className="github__table-body--link"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <i className="fas fa-code-branch"></i>
+                    </a>
+                  </td>
+                  <td className="github__table-body--content">
+                    {item.updated_at.slice(0, 10)}
+                  </td>
+                  <td className="github__table-body--content">
+                    {item.stargazers_count} <i className="fas fa-star"></i>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
--- src/components/views/Blog.js
+++ src/components/views/Blog.js
@@ -7,7 +7,22 @@ class Blog extends React.Component {
     return (
       <div className="page__blog">
         <Navigation />
-        <Content></Content>
+        <Content>
+          <div className="blog__coming-soon">
+            <h1 className="blog__header">Blog Coming Soon</h1>
+            <a
+              href="https://www.gatsbyjs.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://www.gatsbyjs.org/static/gatsby-icon-4a9773549091c227cd2eb82ccd9c5e3a.png"
+                alt="gatsby.js"
+                className="blog__gatsby"
+              />
+            </a>
+          </div>
+        </Content>
       </div>
     );
   }
